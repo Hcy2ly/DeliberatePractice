de21339694fef875160373b67b727803e0a3de1d
Liaoying <iaoying1@duiba.com.cn>
2021-09-27 00:15:59 +0800

    fix： leetcode 快排

--- Leetcode__/sortedSquares.js
+++ Leetcode__/sortedSquares.js
@@ -52,30 +52,52 @@ const doubleFn = (num) => {
 
 // *************** 上面不对 *******************************
 
+
 // 用 Map 对象试试
 var sortedSquares = function (nums) {
   const map = new Map()
-
   for (let i = 0; i < nums.length; i++) {
     map.set(nums[i], doubleFn(nums[i]))
   }
-  // FIXME: 排序有问题
-  return insertionSort(map.values())
+
+  // map.values() 返回的是一个数组 但是实际不是Array类型  没有Array原型链上的方法
+
+  return quickSort([...map.values()])
 }
 
-function insertionSort(arr) {
-  for (let i = 1, len = arr.length; i < len; i++) {
-    const temp = arr[i];
-    const insertIndex = binarySearch(arr, i - 1, arr[i]);
-
-    for (let preIndex = i - 1; preIndex >= insertIndex; preIndex--) {
-      arr[preIndex + 1] = arr[preIndex];
+// 每次选择最左边的数作为基数 - 快排 - 二分法 - 以中间值作为基准，左边push小于自己的 右边push大于自己的  然后递归push 然后最后就是有序数组。
+function quickSort(arr) {
+  if (arr.length < 2) { return arr; }
+  // 定义左指针
+  var left = 0;
+  // 定义右指针
+  var right = arr.length - 1;
+  //开启每一轮的排序
+  while (left < right) {
+    // 寻找右边比arr[0]小的数的下标
+    while (arr[right] >= arr[0] && left < right) {
+      right = right - 1;
     }
-    arr[insertIndex] = temp;
+    // 寻找左边比arr[0]大的数的下标
+    while (arr[left] <= arr[0] && left < right) {
+      left++;
+    }
+    //当左边指针与右边指针相遇后，交换arr[0]与当前两个指针所在的元素
+    if (right == left) {
+      let mid = arr[right];
+      arr[right] = arr[0];
+      arr[0] = mid;
+      break;
+    }
+    // 当左指针小于右指针的位置，交换两个指针当前位置的元素
+    let tem = arr[right];
+    arr[right] = arr[left];
+    arr[left] = tem;
   }
-
-  return arr;
+  //递归实现
+  return quickSort(arr.slice(0, left)).concat(arr.slice(left, right + 1)).concat(quickSort(arr.slice(right + 1)));
 }
+
 
 const arr = [-4, -1, 0, 2, 4, 6]
 console.log(sortedSquares(arr))
